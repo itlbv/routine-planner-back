e0a5eccab46934f9676d7f61a8fcfb30c0147eef
itlbv <artem.v.fokin@gmail.com>
2019-11-04 20:20:53 +0100

    Add NotFoundException, create util package

---
+++ src/main/java/com/itlbv/routineplannerback/util/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.itlbv.routineplannerback.util;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
