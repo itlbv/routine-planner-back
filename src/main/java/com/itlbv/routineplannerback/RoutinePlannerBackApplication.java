704dc86f394d11151a75e25c790cb3133d948bf6
itlbv <artem.v.fokin@gmail.com>
2019-11-04 12:03:45 +0100

    Init commit

---
+++ src/main/java/com/itlbv/routineplannerback/RoutinePlannerBackApplication.java
@@ -0,0 +1,13 @@
+package com.itlbv.routineplannerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinePlannerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutinePlannerBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itlbv/routineplannerback/RoutinePlannerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlbv.routineplannerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinePlannerBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
