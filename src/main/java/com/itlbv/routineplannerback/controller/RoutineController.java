59eaea4480235ed30c7ad1fc74fa70afb56e79a5
itlbv <artem.v.fokin@gmail.com>
2019-11-04 21:14:00 +0100

    Add rest RoutineController and SecurityUtil with mock user authorization

---
+++ src/main/java/com/itlbv/routineplannerback/controller/RoutineController.java
@@ -0,0 +1,56 @@
+package com.itlbv.routineplannerback.controller;
+
+import com.itlbv.routineplannerback.model.Routine;
+import com.itlbv.routineplannerback.service.RoutineService;
+import com.itlbv.routineplannerback.util.SecurityUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "routines")
+public class RoutineController {
+
+    private static final Logger log = LoggerFactory.getLogger(RoutineController.class);
+
+    private final RoutineService service;
+
+    @Autowired
+    RoutineController(RoutineService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    void create(@RequestBody Routine routine) {
+        log.info("create " + routine.toString());
+        service.create(routine, SecurityUtil.USER_01_ID);
+    }
+
+    @PutMapping("/{id}")
+    void update(@RequestBody Routine routine, @PathVariable int id) {
+        log.info("update " + id);
+        service.update(routine, SecurityUtil.USER_01_ID);
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable int id) {
+        log.info("delete " + id);
+        service.delete(id, SecurityUtil.USER_01_ID);
+    }
+
+    @GetMapping("/{id}")
+    Routine get(@PathVariable int id) {
+        log.info("get " + id);
+        return service.get(id, SecurityUtil.USER_01_ID);
+    }
+
+    @GetMapping
+    List<Routine> getAll() {
+        log.info("getAll");
+        return service.getAll(SecurityUtil.USER_01_ID);
+    }
+
+}
---
+++ src/main/java/com/itlbv/routineplannerback/util/SecurityUtil.java
@@ -0,0 +1,5 @@
+package com.itlbv.routineplannerback.util;
+
+public class SecurityUtil {
+    public static final int USER_01_ID = 100000;
+}
